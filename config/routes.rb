4893f71f205042da143113512204265ff9e29848
Stan <solve153@gmail.com>
2017-08-15 12:04:50 +0800

    Add event model, events controller and admin::events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  
+  resources :events
+
+  namespace :admin do
+    root "events#index"
+    resources :events
+  end
+
+  root "events#index"
 end
