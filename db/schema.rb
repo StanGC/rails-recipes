09c33d46c243ddac8bc8b291757dd2b895b4c294
Stan <solve153@gmail.com>
2017-08-15 15:14:22 +0800

    add migrate status to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815060041) do
+ActiveRecord::Schema.define(version: 20170815071145) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170815060041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "friendly_id"
+    t.string "status", default: "draft"
     t.index ["friendly_id"], name: "index_events_on_friendly_id", unique: true
   end
 
