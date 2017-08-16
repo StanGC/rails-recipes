6dda287a994da42891c4baa16902a668ca3b12bc
Stan <solve153@gmail.com>
2017-08-16 15:35:50 +0800

    generate model ticket

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,7 +9,7 @@ class Event < ApplicationRecord
   before_validation :generate_friendly_id, :on => :create
 
   belongs_to :category, :optional => true
-
+  has_many :tickets, :dependent => :destroy
 
   def to_param
     self.friendly_id
