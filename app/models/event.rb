9774535e2206305f32023e46abdf5819d87076fc
Stan <solve153@gmail.com>
2017-08-16 13:35:59 +0800

    generate category model and admin/categories controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,14 @@ class Event < ApplicationRecord
   validates_presence_of :name, :friendly_id
   validates_uniqueness_of :friendly_id
   validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
-  
+
   STATUS = ["draft", "public", "private"]
   validates_inclusion_of :status, :in => STATUS
 
   before_validation :generate_friendly_id, :on => :create
+
+  belongs_to :category, :optional => true
+
 
   def to_param
     self.friendly_id
