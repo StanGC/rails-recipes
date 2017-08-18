bf0a8a0481a2c9499719ddd831cef6a3a7238e85
Stan <solve153@gmail.com>
2017-08-18 13:46:17 +0800

    Generate controller registrations and add registrations form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :events
+  resources :events do
+    resources :registrations
+  end
+  
   resource :user
 
   namespace :admin do
