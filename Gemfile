872b4f50cde28366afbc37a9686f7742224a9534
Stan <solve153@gmail.com>
2017-08-28 15:02:14 +0800

    Add gem letter_opener

--- Gemfile
+++ Gemfile
@@ -70,6 +70,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'faker'
+  gem 'letter_opener'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Letter opener
+  config.action_mailer.delivery_method = :letter_opener
 end
